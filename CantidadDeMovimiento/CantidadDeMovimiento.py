638272e7c72b90037dbee62e619cd55b0bbe882d
Gusseppe Bravo <hackersvlc@gmail.com>
2015-06-08 14:58:26 -0500

    Nuevos cambios, fondos nuevos y estadisticas

--- CantidadDeMovimiento/CantidadDeMovimiento.py
+++ CantidadDeMovimiento/CantidadDeMovimiento.py
@@ -6,6 +6,9 @@ from time import sleep
 from threading import Thread
 import sys
 
+ANCHO=1110
+ALTO=622
+
 background_colour = (255,255,255)
 (width, height) = (400, 400)
 drag = 0.999
@@ -14,86 +17,99 @@ gravity = (math.pi, 0.002)
 
 number_of_particles = 5
 
-def addVectors((angle1, length1), (angle2, length2)):
-    x  = math.sin(angle1) * length1 + math.sin(angle2) * length2
-    y  = math.cos(angle1) * length1 + math.cos(angle2) * length2
-    
-    angle = 0.5 * math.pi - math.atan2(y, x)
-    length  = math.hypot(x, y)
+def cargar_imagen(filename,transparent=False):
+        try: imagen=pygame.image.load(filename)
 
-    return (angle, length)
+        except pygame.error, message:
+                raise SystemExit, message
+
+        imagen=imagen.convert()
+        if transparent:
+                color=imagen.get_at((0,0))
+                imagen.set_colorkey(color, RLEACCEL)
+        return imagen
+
+def addVectors((angle1, length1), (angle2, length2)):
+         x  = math.sin(angle1) * length1 + math.sin(angle2) * length2
+         y  = math.cos(angle1) * length1 + math.cos(angle2) * length2
+
+         angle = 0.5 * math.pi - math.atan2(y, x)
+         length = math.hypot(x, y)
+
+         return (angle, length)
 
 def findParticle(particles, x, y):
-    for p in particles:
-        if math.hypot(p.x-x, p.y-y) <= p.size:
-            return p
-    return None
+         for p in particles:
+                  if math.hypot(p.x-x, p.y-y) <= p.size:
+                                return p
+         return None
 
 def collide(p1, p2):
-    dx = p1.x - p2.x
-    dy = p1.y - p2.y
-    
-    dist = math.hypot(dx, dy)
-    if dist < p1.size + p2.size:
-        tangent = math.atan2(dy, dx)
-        angle = 0.5 * math.pi + tangent
+         dx = p1.x - p2.x
+         dy = p1.y - p2.y
 
-        angle1 = 2*tangent - p1.angle
-        angle2 = 2*tangent - p2.angle
-        speed1 = p2.speed*elasticity
-        speed2 = p1.speed*elasticity
+         dist = math.hypot(dx, dy)
+         if dist < p1.size + p2.size:
+                  tangent = math.atan2(dy, dx)
+                  angle = 0.5 * math.pi + tangent
 
-        (p1.angle, p1.speed) = (angle1, speed1)
-        (p2.angle, p2.speed) = (angle2, speed2)
+                  angle1 = 2*tangent - p1.angle
+                  angle2 = 2*tangent - p2.angle
+                  speed1 = p2.speed*elasticity
+                  speed2 = p1.speed*elasticity
 
-        p1.x += math.sin(angle)
-        p1.y -= math.cos(angle)
-        p2.x -= math.sin(angle)
-        p2.y += math.cos(angle)
+                  (p1.angle, p1.speed) = (angle1, speed1)
+                  (p2.angle, p2.speed) = (angle2, speed2)
+
+                  p1.x += math.sin(angle)
+                  p1.y -= math.cos(angle)
+                  p2.x -= math.sin(angle)
+                  p2.y += math.cos(angle)
 
 class Particle():
-    def __init__(self, (x, y), size):
-        self.x = x
-        self.y = y
-        self.size = size
-        self.colour = (0, 0, 255)
-        self.thickness = 0
-        self.speed = 0
-        self.angle = 0
+         def __init__(self, (x, y), size):
+                  self.x = x
+                  self.y = y
+                  self.size = size
+                  self.colour = (0, 0, 255)
+                  self.thickness = 0
+                  self.speed = 0
+                  self.angle = 0
 
-    def display(self):
-        pygame.draw.circle(screen, self.colour, (int(self.x), int(self.y)), self.size, self.thickness)
+         def display(self):
+                  pygame.draw.circle(pantalla, self.colour, (int(self.x), int(self.y)), self.size, self.thickness)
 
-    def move(self):
-        (self.angle, self.speed) = addVectors((self.angle, self.speed), gravity)
-        self.x += math.sin(self.angle) * self.speed
-        self.y -= math.cos(self.angle) * self.speed
-        self.speed *= drag
+         def move(self):
+                  (self.angle, self.speed) = addVectors((self.angle, self.speed), gravity)
+                  self.x += math.sin(self.angle) * self.speed
+                  self.y -= math.cos(self.angle) * self.speed
+                  self.speed *= drag
 
-    #rebotar
-    def bounce(self):
-        if self.x > width - self.size:
-            self.x = 2*(width - self.size) - self.x
-            self.angle = - self.angle
-            self.speed *= elasticity
+         #rebotar
+         def bounce(self):
+                  if self.x > width - self.size:
+                                self.x = 2*(width - self.size) - self.x
+                                self.angle = - self.angle
+                                self.speed *= elasticity
 
-        elif self.x < self.size:
-            self.x = 2*self.size - self.x
-            self.angle = - self.angle
-            self.speed *= elasticity
+                  elif self.x < self.size:
+                                self.x = 2*self.size - self.x
+                                self.angle = - self.angle
+                                self.speed *= elasticity
 
-        if self.y > height - self.size:
-            self.y = 2*(height - self.size) - self.y
-            self.angle = math.pi - self.angle
-            self.speed *= elasticity
+                  if self.y > height - self.size:
+                                self.y = 2*(height - self.size) - self.y
+                                self.angle = math.pi - self.angle
+                                self.speed *= elasticity
 
-        elif self.y < self.size:
-            self.y = 2*self.size - self.y
-            self.angle = math.pi - self.angle
-            self.speed *= elasticity
+                  elif self.y < self.size:
+                                self.y = 2*self.size - self.y
+                                self.angle = math.pi - self.angle
+                                self.speed *= elasticity
 
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Cantidad de movimiento')
+#screen = pygame.display.set_mode((width, height))
+pantalla=pygame.display.set_mode((ANCHO,ALTO))
+#pygame.display.set_caption('Cantidad de movimiento')
 
 #ver luego
 #number_of_particles = 5
@@ -102,70 +118,94 @@ pygame.display.set_caption('Cantidad de movimiento')
 my_particles = []
 
 for n in range(number_of_particles):
-    size = random.randint(10, 20)
-    x = random.randint(size, width-size)
-    y = random.randint(size, height-size)
+         size = random.randint(10, 20)
+         x = random.randint(size, width-size)
+         y = random.randint(size, height-size)
 
-    particle = Particle((x, y), size)
-    particle.speed = random.random()
-    particle.angle = random.uniform(0, math.pi*2)
+         particle = Particle((x, y), size)
+         particle.speed = random.random()
+         particle.angle = random.uniform(0, math.pi*2)
 
-    my_particles.append(particle)
+         my_particles.append(particle)
 
-def estadistica():
+#def estadistica():
 
-    sys.stdout.write( '\r{: >3}'.format( "Velocidad : " + str(particle.speed) + " Angulo : " + str(particle.angle) ) )
-    #sys.stdout.write( '  ' )
-    #sys.stdout.write( '\r{: >3}'.format( particle.angle ) )
-    sys.stdout.flush()
-    sleep( 0.05 )
-    
-class RollAnimation ( Thread ):
-    activated = True
-    number = None
+         #sys.stdout.write( '\r{: >3}'.format( "Velocidad : " + str(particle.speed) + " Angulo : " + str(particle.angle) ) )
+         ##sys.stdout.write( '  ' )
+         ##sys.stdout.write( '\r{: >3}'.format( particle.angle ) )
+         #sys.stdout.flush()
+         #sleep( 0.05 )
 
-    def run ( self):
-        while self.activated:
-            #self.number = randint( 1, 100 )
-            estadistica()
+#class RollAnimation ( Thread ):
+         #activated = True
+         #number = None
 
-selected_particle = None
-running = True
-#Estadisticas
+         #def run ( self):
+                  #while self.activated:
+                                ##self.number = randint( 1, 100 )
+                                #estadistica()
 
+def main():
 
-pygame.display.set_caption('Kit de Fisica')
+    selected_particle = None
+    running = True
 
-print 'Estadistica:\n'
-while running:
-    #t.start()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            (mouseX, mouseY) = pygame.mouse.get_pos()
-            #print 'Hola'
-            selected_particle = findParticle(my_particles, mouseX, mouseY)
-        elif event.type == pygame.MOUSEBUTTONUP:
-            t = RollAnimation()
-            t.start()
-            selected_particle = None
-            t.activated = False
+    #Estadisticas
+    pygame.display.set_caption("Cantidad de movimiento")
+    fondo=cargar_imagen("images/fondoCantidad.jpg")
+    #pygame.mixer.music.load("sounds/carrera.mp3")
+    time=0
+    arrancar=False
+    press=0
+    fuente=pygame.font.Font(None,20)
+    reloj=pygame.time.Clock()
 
-    if selected_particle:
-        (mouseX, mouseY) = pygame.mouse.get_pos()
-        dx = mouseX - selected_particle.x
-        dy = mouseY - selected_particle.y
-        selected_particle.angle = 0.5*math.pi + math.atan2(dy, dx)
-        selected_particle.speed = math.hypot(dx, dy) * 0.1
+    pygame.display.set_caption('Kit de Fisica')
 
-    screen.fill(background_colour)
+    #print 'Estadistica:\n'
+    while running:
+         #tick=reloj.tick(60)
+         for event in pygame.event.get():
+              if event.type == pygame.QUIT:
+                    running = False
+              elif event.type == pygame.MOUSEBUTTONDOWN:
+                (mouseX, mouseY) = pygame.mouse.get_pos()
+                #print 'Hola'
+                selected_particle = findParticle(my_particles, mouseX, mouseY)
+              elif event.type == pygame.MOUSEBUTTONUP:
+                #t = RollAnimation()
+                #time=time+(tick/1000.0)
+                #t.start()
+                selected_particle = None
+                #t.activated = False
+                datosParticula = "Posicion (x, y): (%4f,%4f) m  Velocidad: %3f m/s Angulo: %f m" %(particle.x,
+                        particle.y, particle.speed,particle.angle)
+                datoTiempo = "Tiempo transcurrido %f s"%(time)
+                mensaje1=fuente.render(datosParticula,1,(0,0,0))
+                mensaje3=fuente.render(datoTiempo,1,(0,0,0))
+                #----------------------------------------------------
+                pantalla.blit(fondo, (0,0))
+                pantalla.blit(mensaje1,(5,560))
+                pantalla.blit(mensaje3,(5,600))
 
-    for i, particle in enumerate(my_particles):
-        particle.move()
-        particle.bounce()
-        for particle2 in my_particles[i+1:]:
-            collide(particle, particle2)
-        particle.display()
+         if selected_particle:
+                (mouseX, mouseY) = pygame.mouse.get_pos()
+                dx = mouseX - selected_particle.x
+                dy = mouseY - selected_particle.y
+                selected_particle.angle = 0.5*math.pi + math.atan2(dy, dx)
+                selected_particle.speed = math.hypot(dx, dy) * 0.1
 
-    pygame.display.flip()
+         #screen.fill(background_colour)
+
+         for i, particle in enumerate(my_particles):
+              particle.move()
+              particle.bounce()
+              for particle2 in my_particles[i+1:]:
+                            collide(particle, particle2)
+              particle.display()
+
+         pygame.display.flip()
+
+if __name__=='__main__':
+        pygame.init()
+        main()
